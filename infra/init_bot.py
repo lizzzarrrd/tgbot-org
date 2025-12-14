ed8c14654f525cae92f12114ca8247213e5e2e0a
Egor Kosov <eakosov@edu.hse.ru>
2025-12-14 19:41:40 +0300

    create bot_token

--- infra/init_bot.py
+++ infra/init_bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from handlers.commands import router as commands_router
 import os
 
-API_TOKEN = os.getenv("BOT_TOKEN")
+API_TOKEN = '8305223003:AAGGk5NoBE2l01OdjA6pFFXUEXleqZLviFU'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
