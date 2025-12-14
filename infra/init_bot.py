d9ca34f1eaea2db39047d5b9d735606f37aaaf8f
Pavel Tishutin <pstishutin@edu.hse.ru>
2025-12-14 21:45:28 +0300

    from that moment handler is called from adapters folder

--- infra/init_bot.py
+++ infra/init_bot.py
@@ -1,9 +1,21 @@
 from aiogram import Bot, Dispatcher
-from handlers.commands import router as commands_router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from adapters.handlers.start import StartHandler
+from adapters.handlers.send_message import MessageSender
+#импротируем нужные библиотеки потом из инита лучше
+import asyncio
 import os
 
 API_TOKEN = '8305223003:AAGGk5NoBE2l01OdjA6pFFXUEXleqZLviFU'
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
-dp.include_router(commands_router)
+# Ручка для команды /start
+
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    sender = MessageSender(bot)
+    await StartHandler(sender=sender).handle(message)
+
