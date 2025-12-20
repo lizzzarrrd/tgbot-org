03be075542b0d5e307eb995de58e587704541b40
Egor Prakhov <prakhov.egor@gmail.com>
2025-12-20 18:55:53 +0300

    Соеденил функционал исправление мероприятия(бота и парсера) и обновил кнопки

--- tg_bot/domain/buttons/transform_event_button.py
+++ tg_bot/domain/buttons/transform_event_button.py
@@ -2,12 +2,14 @@ from enum import StrEnum
 
 
 class TransformEventButton(StrEnum):
-    TRANSORM_DATE: str = "Поменять дату"
-    TRANSORM_TIME: str = "Поменять время"
+    TRANSORM_DATE_START: str = "Поменять дату начала"
+    TRANSORM_DATE_END: str = "Поменять дату конца"
     TRANSORM_NAME: str = "Поменять название"
     TRANSORM_DESCRIPTION: str = "Поменять описание"
-    TRANSORM_DATE_PUSHED: str = "Пожалуйста, введите корректную дату. Пример: 2023-09-15"
-    TRANSORM_TIME_PUSHED: str = "Пожалуйста, введите корректное время. Пример: 14:00"
+    TRANSORM_LOCATION: str = "Поменять место"
+    TRANSORM_DATE_PUSHED: str = "Пожалуйста, введите корректную дату. Пример: 2023-09-15 14:00"
     TRANSORM_NAME_PUSHED: str = "Пожалуйста, введите корректное название события."
     TRANSORM_DESCRIPTION_PUSHED: str = "Пожалуйста, введите корректное описание события."
+    TRANSORM_LOCATION_PUSHED: str = "Пожалуйста, введите корректное место события."
+
 
--- tg_bot/domain/state.py
+++ tg_bot/domain/state.py
@@ -3,10 +3,11 @@ from aiogram.fsm.state import StatesGroup, State
 
 class MessageProcessingStates(StatesGroup):
     
-    EDITING_DATE = State()
-    EDITING_TIME = State()
+    EDITING_DATE_START = State()
+    EDITING_DATE_END = State()
     EDITING_NAME = State()
     EDITING_DESCRIPTION = State()
+    EDITING_LOCATION = State()
 
 
     
