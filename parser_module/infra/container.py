3d0b69b508f496ee5307c0bf2fb830f94ab958c1
Egor Kosov <eakosov@edu.hse.ru>
2025-12-20 09:54:16 +0300

    Remove Python cache files and update project structure

--- parser_module/infra/container.py
+++ parser_module/infra/container.py
@@ -1,16 +1,17 @@
 from parser_module.adapters.llm_adapter import YandexGptAdapter
 from parser_module.use_cases.parse_service import ParseService
 from parser_module.use_cases.event_service import EventService
-from parser_module.infra.settings import Settings
+from core.config import settings
+
+YANDEX_API_KEY = settings.yandex_api_key
+YANDEX_MODEL_URI = settings.yandex_model_uri
 
 class Container:
-    def __init__(self):
-        self.settings = Settings()
-
+    
     def llm(self) -> YandexGptAdapter:
         return YandexGptAdapter(
-            api_key=self.settings.YANDEX_API_KEY,
-            model_uri=self.settings.YANDEX_MODEL_URI,
+            api_key=YANDEX_API_KEY,
+            model_uri=YANDEX_MODEL_URI,
         )
 
     def parse_service(self) -> ParseService:
--- tg_bot/infra/init_bot.py
+++ tg_bot/infra/init_bot.py
@@ -1,17 +1,13 @@
 from aiogram import Router, Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from dotenv import load_dotenv
-import os
 
-load_dotenv(".env.bot")
+from core.config import settings
+
 storage = MemoryStorage()
 
+API_TOKEN_BOT = settings.api_token_bot
 
-API_TOKEN: str = os.getenv("API_TOKEN_BOT")
-if not API_TOKEN:
-    raise RuntimeError("API_TOKEN_BOT not found in environment variables")
-
-bot: Bot = Bot(token=API_TOKEN)
+bot: Bot = Bot(token=API_TOKEN_BOT)
 dp: Dispatcher = Dispatcher()
 
 router: Router = Router()
