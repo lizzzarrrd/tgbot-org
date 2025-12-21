25f618941d91b315b603fd434a89b7f82fd53f27
Pavel Tishutin <pstishutin@edu.hse.ru>
2025-12-21 21:34:26 +0300

    one man changed my code and the whole project structure too

--- tests/tg/test_change_event_route.py
+++ tests/tg/test_change_event_route.py
@@ -1,24 +1,26 @@
 import pytest
 from unittest.mock import AsyncMock
 from tg_bot.adapters import ChangeEventHandler
-from tg_bot.domain import TransformEventButton, MessageProcessingStates
+from tg_bot.domain import TransformEventButton
 
 
 @pytest.mark.asyncio
 class TestChangeEventHandler:
+
     async def test_init(self):
         mock_sender = AsyncMock()
         handler = ChangeEventHandler(mock_sender)
-
         assert handler.sender == mock_sender
 
-    async def test_handle_change_date(self):
+    async def test_handle_change_name(self):
         mock_sender = AsyncMock()
         handler = ChangeEventHandler(mock_sender)
         callback = AsyncMock()
-        callback.data = TransformEventButton.TRANSORM_DATE
+        callback.data = TransformEventButton.TRANSORM_NAME.value
+        callback.message = AsyncMock()
+        callback.message.edit_reply_markup = AsyncMock()
         state = AsyncMock()
         await handler.handle_for_event_changing_info(callback, state)
-        state.set_state.assert_called_once_with(
-            MessageProcessingStates.EDITING_DATE)
+        assert mock_sender.send_text.called is True
         assert callback.answer.called is True
+        assert state.set_state.called is True
