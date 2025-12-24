e7587a1985bedcbed79b80da81c0c2b419b7c239
Polina Batkilina <poina.batkilina@gmail.com>
2025-12-24 16:45:19 +0300

    deleted previous realization of oauth

--- calendar_actions/__init__.py
+++ calendar_actions/__init__.py
@@ -1,14 +1,3 @@
-from .google_authentication import (
-    DEFAULT_SCOPES,
-    GoogleAuthNotInstalled,
-    OAuthClient,
-    get_authorization_url,
-    exchange_code_for_credentials,
-    refresh_credentials,
-    credentials_to_json_dict,
-    credentials_from_json_dict,
-)
-
 from .ics_generator import (
     IcsEvent,
     ics_content,
@@ -30,13 +19,7 @@ from .google_calendar import (
 
 __all__ = [
     "DEFAULT_SCOPES",
-    "GoogleAuthNotInstalled",
     "OAuthClient",
-    "get_authorization_url",
-    "exchange_code_for_credentials",
-    "refresh_credentials",
-    "credentials_to_json_dict",
-    "credentials_from_json_dict",
     "IcsEvent",
     "ics_content",
     "build_ics_event_from_project_event",
